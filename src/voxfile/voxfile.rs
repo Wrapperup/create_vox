49c1b8ddd512b59f7a32aecac25856b7d941dafc
Wrapperup <wrapperup4@gmail.com>
2022-01-08 11:51:28 -0500

    Change visibility of get_model_by_id

--- src/voxfile/voxfile.rs
+++ src/voxfile/voxfile.rs
@@ -241,7 +241,7 @@ impl VoxFile {
         return id
     }
 
-    fn get_model_by_id(&self, id: i32) -> Option<&Model> {
+    pub fn get_model_by_id(&self, id: i32) -> Option<&Model> {
         for model in self.models.iter() {
             if model.id == id {
                 return Some(model);
